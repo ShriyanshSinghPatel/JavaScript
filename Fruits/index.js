bb9c1ae05a14d88eeec7a32a6fcc6665ec8bee54
ShriyanshSinghPatel <shriyanshpatel25@gmail.com>
2025-07-04 21:59:58 +0530

    asd

--- Fruits/index.js
+++ Fruits/index.js
@@ -3,7 +3,6 @@ const bananadata = require("./banana")
 const orangedata = require("./orange")
 // console.log(appledata);
 // console.log(bananadata);
-
 // console.log(orangedata);
 const data=[appledata,bananadata,orangedata];
 module.exports=data
